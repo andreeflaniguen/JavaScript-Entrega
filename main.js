262e7de509c0dab782bf9276b4d8fef6b7760f0a
andree flaniguen <andreeflaniguen@gmail.com>
2024-01-24 08:48:37 -0300

    Tercera entrega JS

--- main.js
+++ main.js
@@ -1,35 +1,36 @@
 
-let catalogoCajas = [
+  let catalogoCajas = [
     { modelo: "Argo", contenido: "Vinos", precio: 1500 },
     { modelo: "Greta", contenido: "Té", precio: 1300 },
     { modelo: "Justa", contenido: "Cafe", precio: 1200 },
   ];
-  
+
   function buscarCajasPorModelo(modelo) {
     for (let cajas of catalogoCajas) {
       if (cajas.modelo.toLowerCase() === modelo.toLowerCase()) {
         return cajas;
       }
     }
-    return null; 
+    return null;
   }
-  
 
   function mostrarInformacionCajas(cajas) {
+    let resultado = document.getElementById("resultado");
+
     if (cajas) {
-      console.log(`Modelo: ${cajas.modelo}`);
-      console.log(`Contenido: ${cajas.contenido}`);
-      console.log(`Precio: $${cajas.precio}`);
+      resultado.innerHTML = `
+        <p>Modelo: ${cajas.modelo}</p>
+        <p>Contenido: ${cajas.contenido}</p>
+        <p>Precio: $${cajas.precio}</p>
+      `;
     } else {
-      console.log("Caja no encontrado en el catálogo.");
+      resultado.innerHTML = "<p>Caja no encontrado en el catálogo.</p>";
     }
   }
-  
 
+  // Obtener el modelo del usuario
   let modeloUsuario = prompt("Ingrese la caja que desea buscar:");
-  
 
+  // Buscar la caja y mostrar la información
   let cajasEncontrado = buscarCajasPorModelo(modeloUsuario);
-  
   mostrarInformacionCajas(cajasEncontrado);
-  
